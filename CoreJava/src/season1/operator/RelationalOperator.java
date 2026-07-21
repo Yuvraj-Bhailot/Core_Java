57982b1eab97ced07b3b35ee1faea2f804e87a61
Yuvi <yuvilearner@gmail.com>
2026-07-21 19:08:49 +0530

    added relational operator

---
+++ CoreJava/src/season1/operator/RelationalOperator.java
@@ -0,0 +1,17 @@
+package season1.operator;
+
+public class RelationalOperator {
+	public static void main(String[] args) {
+		// relational operator/Comparison
+		
+		double num1=10.0;
+		double num2=25.10;
+		
+		System.out.println("num1("+num1+") > num2("+num2+"): " + (num1>num2));
+		System.out.println("num1("+num1+") < num2("+num2+"): " + (num1<num2));
+		System.out.println("num1("+num1+") >= num2("+num2+"): " + (num1>=num2));
+		System.out.println("num1("+num1+") <= num2("+num2+"): " + (num1<=num2));
+		System.out.println("num1("+num1+") == num2("+num2+"): " + (num1==num2));
+		System.out.println("num1("+num1+") != num2("+num2+"): " + (num1!=num2));
+	}
+}
