891f13f49f491d65d7102a71963950d7125ae69c
Yuvi <yuvilearner@gmail.com>
2026-07-22 21:20:02 +0530

    typecasting

--- CoreJava/src/season1/TypeCasting.java
+++ CoreJava/src/season1/TypeCasting.java
@@ -4,18 +4,18 @@ public class TypeCasting {
 	public static void main(String[] args) {
 		// Typecasting means converting one datatype to another datatype
 		// Primitive Typecasting
-		
+
 		int num = 38;
 		double num2 = num; // Widening
 
 		System.out.println("Integer: " + num);
 		System.out.println("Integer -> Double: " + num2);
 		// converting smaller datatype into larger one
-		
+
 		System.out.println("--------------------");
-		
-		char c=(char)num; // Narrowing
-		
+
+		char c = (char) num; // Narrowing
+
 		System.out.println("Integer: " + num);
 		System.out.println("Integer -> Character: " + c);
 		// converting larger datatype into smaller one
