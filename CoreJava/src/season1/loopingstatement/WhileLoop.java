b8ade27758b344ba490b0522cb10d1b3b8e4a4b0
Yuvi <yuvilearner@gmail.com>
2026-07-23 23:55:50 +0530

    while loop

---
+++ CoreJava/src/season1/loopingstatement/WhileLoop.java
@@ -0,0 +1,14 @@
+package season1.loopingstatement;
+
+public class WhileLoop {
+	public static void main(String[] args) {
+		// while loop: entry controlled loop
+		int i = 1; // initialization
+
+		// condition
+		while (i <= 5) {
+			System.out.println("Looping statement: " + i);
+			i++; // iterator
+		}
+	}
+}
