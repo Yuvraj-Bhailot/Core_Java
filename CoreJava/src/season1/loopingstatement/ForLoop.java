b442eb1cf9e0638b406ab0a2d2b6ec2528baf23b
Yuvi <yuvilearner@gmail.com>
2026-07-24 00:00:35 +0530

    for loop

---
+++ CoreJava/src/season1/loopingstatement/ForLoop.java
@@ -0,0 +1,12 @@
+package season1.loopingstatement;
+
+public class ForLoop {
+	public static void main(String[] args) {
+		// for loop: entry controlled loop
+
+		// (initialization; condition; iterator) are in same line
+		for (int i = 1; i <= 10; i++) {
+			System.out.println("5 * " + i + " = " + (5 * i));
+		}
+	}
+}
