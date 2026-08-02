f6794bbcaa2f6a11b4e0dbe16e3e1e075e4b7b7e
Yuvi <yuvilearner@gmail.com>
2026-08-02 23:33:05 +0530

    formatted

--- CoreJava/src/season3/array/Array2D.java
+++ CoreJava/src/season3/array/Array2D.java
@@ -3,13 +3,13 @@ package season3.array;
 public class Array2D {
 	public static void main(String[] args) {
 		// 2D array
-		int[][] arr= {{1,2,3},{4,5,6},{7,8,9}};
-		
+		int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+
 		// printing 2D array
 		System.out.println("2D Array: ");
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr[i].length; j++) {
-				System.out.print(arr[i][j]+" ");
+				System.out.print(arr[i][j] + " ");
 			}
 			System.out.println();
 		}
