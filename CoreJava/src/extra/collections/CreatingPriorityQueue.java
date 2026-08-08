d2fcaf805fdf8c58899746869d86c1a66b74b241
Yuvi <yuvilearner@gmail.com>
2026-08-08 19:56:31 +0530

    Priority Queue

--- CoreJava/src/extra/collections/CreatingPriorityQueue.java
+++ CoreJava/src/extra/collections/CreatingPriorityQueue.java
@@ -1,5 +1,27 @@
 package extra.collections;
 
-public class CreatingPriorityQueue {
+/* # Priority Queue<C>
+ * -	Priority Queue is implementing class of Queue interface.
+ * -	Insertion order not maintain.
+ * -	It is unordered collection
+ * -	Only homogeneous elements are allowed.
+ * -	If we add heterogeneous elements we will get ClassCastException.
+ * -	Duplicates are allowed.
+ * */
 
+import java.util.PriorityQueue;
+
+public class CreatingPriorityQueue {
+	public static void main(String[] args) {
+		PriorityQueue queue = new PriorityQueue();
+		
+		queue.add(10);
+		queue.add(30);
+		queue.add(20);
+		queue.add(50);
+		queue.add(40);
+		
+		System.out.println("queue: ");
+		System.out.println(queue);
+	}
 }
