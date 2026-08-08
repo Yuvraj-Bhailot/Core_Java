32568d0e42f93d7ce4a8c768a6412ca6131bf0d2
Yuvi <yuvilearner@gmail.com>
2026-08-08 19:56:00 +0530

    hash set

--- CoreJava/src/extra/collections/CreatingHashSet.java
+++ CoreJava/src/extra/collections/CreatingHashSet.java
@@ -1,5 +1,26 @@
 package extra.collections;
 
-public class CreatingHashSet {
+/* # characteristics of HashSet :- 
+ * -	No duplicate.
+ * -	No multiple null value.
+ * -	Unordered collection.
+ * -	No index value.
+ * -	No insertion order.
+ * */
 
+import java.util.HashSet;
+
+public class CreatingHashSet {
+	public static void main(String[] args) {
+		HashSet hashSet = new HashSet();
+		
+		hashSet.add("One");
+		hashSet.add(934);
+		hashSet.add(true);
+		hashSet.add('F');
+		hashSet.add("Piece");
+		
+		System.out.println("hashSet: ");
+		System.out.println(hashSet);
+	}
 }
