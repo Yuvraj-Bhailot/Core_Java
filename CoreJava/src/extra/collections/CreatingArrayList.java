0cd4417f824c5ac4c571f4f244c37df4f91aa48f
Yuvi <yuvilearner@gmail.com>
2026-08-08 19:55:50 +0530

    Array list

--- CoreJava/src/extra/collections/CreatingArrayList.java
+++ CoreJava/src/extra/collections/CreatingArrayList.java
@@ -1,5 +1,25 @@
 package extra.collections;
 
-public class CreatingArrayList {
+/* ArrayList is an implementing class of list interface.
+ * It is defined in java.util package.
+ * The characteristics of ArrayList is exactly same as List interface.
+ * */
 
+import java.util.ArrayList;
+
+public class CreatingArrayList {
+	public static void main(String[] args) {
+		ArrayList arrList = new ArrayList();
+		
+		arrList.add(15);
+		arrList.add("Hello");
+		arrList.add('B');
+		arrList.add(637454.154);
+		arrList.add(true);
+		arrList.add(324);
+		arrList.add("Java");
+		
+		System.out.println("arrList: ");
+		System.out.println(arrList);		
+	}
 }
